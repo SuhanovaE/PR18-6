a3a9f86fc9bce7dfb6c83167f7e878a73c5aff6b
SuhanovaE <98471539+SuhanovaE@users.noreply.github.com>
2022-05-26 20:16:06 +0300

    Add files via upload

---
+++ SinCos/SinCos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinCos
+{
+    public class SiCo
+    {
+        public double x;
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public SiCo()
+        {
+            x = 0;
+        }
+        public SiCo(double a)
+        {
+            x = a;
+        }
+        public override string ToString()
+        {
+            return $"{x}";
+        }
+        public double Calc()
+        {
+            return Math.Sin(X) + Math.Cos(2 * X);
+        }
+    }
+}
---
+++ WPF/WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SinCos;
+
+namespace WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            SiCo a = new SiCo();
+            SiCo b = new SiCo();
+            SiCo c = new SiCo();
+            a.X = Convert.ToDouble(txtA.Text);
+            b.X = Convert.ToDouble(txtB.Text);
+            c.X = Convert.ToDouble(txtC.Text);
+            txbA.Text = Convert.ToString(Math.Round(a.Calc(), 5));
+            txbB.Text = Convert.ToString(Math.Round(b.Calc(), 5));
+            txbC.Text = Convert.ToString(Math.Round(c.Calc(), 5));
+            txbMin.Text = Math.Round(Math.Min(Math.Min(a.Calc(), b.Calc()), c.Calc()), 5).ToString();
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txbA.Text = "";
+            txbB.Text = "";
+            txbC.Text = "";
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+            txbMin.Text = "";
+        }
+    }
+}
